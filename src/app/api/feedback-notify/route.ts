8829dc58ad32960713a35961bcea144238315989
C Bhanu Koushik <cbhanukoushik15@gmail.com>
2026-06-20 16:24:45 +0530

    Fixed Feedback Notification

--- src/app/api/feedback-notify/route.ts
+++ src/app/api/feedback-notify/route.ts
@@ -1,13 +1,11 @@
 export async function POST(req: Request) {
-
   const data = await req.json();
 
-  await fetch(
+  const response = await fetch(
     "https://ntfy.sh/bhanu-portfolio-visits-2026",
     {
       method: "POST",
-      body: `
-⭐ New Portfolio Feedback
+      body: `⭐ New Portfolio Feedback
 
 Name: ${data.name}
 
@@ -15,14 +13,12 @@ Rating: ${data.rating}/5
 
 Feedback:
 ${data.feedback}
-
-Time:
-${new Date().toLocaleString()}
 `
     }
   );
 
   return Response.json({
-    success: true
+    success: true,
+    status: response.status,
   });
 }
